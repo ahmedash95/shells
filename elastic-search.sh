5d400fccf7c16bfc42a627ca65f1f3c3bea14b2c
Ahmed Ashraf <ahmed29329@gmail.com>
2016-11-12 21:28:53 +0200

    Create elastic-search.sh

---
+++ elastic-search.sh
@@ -0,0 +1,18 @@
+# install open jdk
+
+sudo apt-get install openjdk-6-jre
+
+# install java
+
+sudo add-apt-repository ppa:webupd8team/java
+
+sudo apt-get update
+
+sudo apt-get install oracle-java7-installer
+
+# install elastic search
+
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.7.deb
+
+dpkg -i elasticsearch-0.90.7.deb
+
